7172ffe1dfe6ac6924ff93307bad1c7a31b7013c
Ihor EzSup <opsig4@gmail.com>
2024-08-22 18:38:11 +0300

    basic realisation created

---
+++ ChatSignalR.Server/ChatSignalR.Server/Data/ChatDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace ChatSignalR.Server.Data
+{
+    public class ChatDbContext : DbContext
+    {
+
+        public ChatDbContext(DbContextOptions<ChatDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ ChatSignalR.Server/ChatSignalR.Server/Program.cs
@@ -0,0 +1,51 @@
+
+using ChatSignalR.Server.Data;
+using ChatSignalR.Server.Hubs;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatSignalR.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddSignalR()
+                .AddAzureSignalR();
+
+            var dbcon = builder.Configuration["ConnectionStrings:DefaultConnection"];
+
+            builder.Services.AddDbContext<ChatDbContext>(
+                options =>
+                {
+                    options.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnection"]);
+                });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapHub<ChatHub>("/chat");
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
